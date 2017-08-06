14635380a9c5ef8dc55fd9c7e4d9099b1d61723c
Fiona Thendean <fionahtt@gmail.com>
2017-08-06 16:00:55 -0400

    Add files via upload

---
+++ levelChooser.py
@@ -0,0 +1,68 @@
+# 15-112, Summer 2, Term Project
+######################################
+# Full name: Fiona Thendean
+# Section: B
+# Andrew ID: fthendea
+######################################
+from tkinter import *
+
+class levelChooser(object):
+    def draw(canvas, data):
+        pass
+    
+    def init(data):
+        pass
+        
+    def mousePressed(event, data):
+        pass
+    
+    def keyPressed(event, data):
+        pass
+    
+    def timerFired(data):
+        pass
+    
+    def redrawAll(canvas, data):
+        draw(canvas, data)
+            
+    def run(self):
+        def redrawAllWrapper(canvas, data):
+            canvas.delete(ALL)
+            canvas.create_rectangle(0, 0, data.width, data.height,
+                                    fill='white', width=0)
+            redrawAll(canvas, data)
+            canvas.update()    
+    
+        def mousePressedWrapper(event, canvas, data):
+            mousePressed(event, data)
+            redrawAllWrapper(canvas, data)
+    
+        def keyPressedWrapper(event, canvas, data):
+            keyPressed(event, data)
+            redrawAllWrapper(canvas, data)
+    
+        def timerFiredWrapper(canvas, data):
+            timerFired(data)
+            redrawAllWrapper(canvas, data)
+            # pause, then call timerFired again
+            canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+        # Set up data and call init
+        class Struct(object): pass
+        data = Struct()
+        data.width = 600
+        data.height = 750
+        data.timerDelay = 100 # milliseconds
+        init(data)
+        # create the root and the canvas
+        root = Toplevel()
+        canvas = Canvas(root, width=data.width, height=data.height)
+        canvas.pack()
+        # set up events
+        root.bind("<Button-1>", lambda event:
+                                mousePressedWrapper(event, canvas, data))
+        root.bind("<Key>", lambda event:
+                                keyPressedWrapper(event, canvas, data))
+        timerFiredWrapper(canvas, data)
+        # and launch the app
+        root.mainloop()  # blocks until window is closed
+        print("bye!")
